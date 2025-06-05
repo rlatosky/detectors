f17559c7595c25ad93a27546b24996fc6ad473bd
Rylen Latosky <rlatosky@posteo.net>
2025-06-05 10:52:46 -0400

    Update analysis program and create export to root file script

--- phyTests/axial_neutrons/analysis.cc
+++ phyTests/axial_neutrons/analysis.cc
@@ -23,38 +23,74 @@ int main(int argc, char** argv) {
 		exit(0);
 	}
 
-	const double r2d = TMath::RadToDeg();
-
+	const double total_events_gemc = 10000.0; // eff_values need to be type float
 	const int neutron_ID = 2112;
 	const int proton_ID = 2212;
 	const int electron_ID = 11;
 	const int positron_ID = -11;
-	double    zpos1      = 536.0;
-	double    zpos2      = 2536.0;
-	double    Mn          = 939.565; // MeV
-	//double    M          = 938.272; // MeV
-	double    ene_cut    = 3;
 	const double PROTONMASS = 928.272; // MeV
 	const double NEUTRONMASS = 939.565;
 	const double ELECTRONMASS = 0.510998;
 	const double PHOTONMASS = 0;
 
-	hipo::reader reader;
-	reader.open(argv[1]);
+	const char* file = argv[1];
+	const char* particle = argv[2];
+	const int kin_energy = atoi(argv[3]);
+	const int hist_bins_and_xlim = atoi(argv[4]);
+	const char* filename_woext = argv[5];
 
+	// Debug
+	cout << "Arguments:" << endl;
+	cout << "argv[1]:\t" << argv[1] << endl;
+	cout << "argv[2]:\t" << argv[2] << endl;
+	cout << "argv[3]:\t" << argv[3] << endl;
+	cout << "argv[4]:\t" << argv[4] << endl;
+
+	// Initialize hipo library
+	hipo::reader reader;
+	reader.open(file);
 	hipo::dictionary factory;
 	reader.readDictionary(factory);
-	factory.show();
+	//factory.show();
 
 	hipo::event event;
-	int         evCounter = 0;
-
+	int evCounter = 0;
 	hipo::bank bMCTrue(factory.getSchema("MC::True"));
 	hipo::bank fluxADC(factory.getSchema("FLUX::adc"));	// get flux entry
 
+	// Initialize graphs
+    TGraph g_efficiency(0);
+
+	// Graph and filename options
+	const char* file_ext = ".root";
+	const char* hist_x_label = "E_dep (MeV)";
+	const char* hist_y_label = "Events";
+	const char* hist_eff_y_label = "Events (with energy cut)";
+	const char* hist_totedep_name = "h_totedep";
+	const char* hist_eff_name = "h_effedep";
+
+	const char* eff_graph_name = "efficiency";
+	std::string eff_graph_title_str = particle;
+	eff_graph_title_str += " - Threshold vs. Efficiency - ";
+	eff_graph_title_str += filename_woext;
+	eff_graph_title_str += ";Thresholds (MeV);Efficiency (N_det/N_events)";
+	const char* eff_graph_title = eff_graph_title_str.c_str();
+
+	std::string h_edep_title_str = particle;
+	h_edep_title_str += " E_dep - ";
+	h_edep_title_str += filename_woext;
+	const char* h_edep_title = h_edep_title_str.c_str();
+
+	// Setup filename for .root file
+	std::string file_save_str = filename_woext;
+	file_save_str += file_ext;
+	const char* file_save = file_save_str.c_str();
+	auto file_out = new TFile(file_save, "Recreate");
+	auto tree = new TTree(file_save,"Tree");
+
 	double M;
-	cout << argv[2][0] << endl;
-	switch (char(argv[2][0])) {	// first letter of second parameter since switches are for ints/chars
+	switch (char(particle[0])) {
+	// first letter of second parameter since switches are only for ints/chars
 		case 'p':
 			M = PROTONMASS;
 			cout << "Mass set to proton mass..." << endl;
@@ -67,50 +103,39 @@ int main(int argc, char** argv) {
 			M = ELECTRONMASS; // MeV
 			cout << "Mass set to electron mass..." << endl;
 			break;
-		case 'g':
+		case 'g':	// Photons are called gammas in GEMC
 			M = 0; // MeV
 			cout << "Mass set to photon mass..." << endl;
 			break;
 		default:
-			M = NEUTRONMASS; // default neutron
+			M = NEUTRONMASS; // default to neutron
 			cout << "Mass set to neutron mass..." << endl;
 			break;
 	}
-	std::string fileSavestr = argv[2];
-	fileSavestr += argv[3];
-	fileSavestr += "-KE.root";
-	const char * fileSave = fileSavestr.c_str();
-	auto       file_out = new TFile(fileSave, "Recreate");
-	auto tree = new TTree(fileSave,"Tree");
 
-	std::string hTotEdepTitlestr = "Total ";
-	hTotEdepTitlestr += argv[2];
-	hTotEdepTitlestr += " E_dep - ";
-	hTotEdepTitlestr += argv[3];
-	hTotEdepTitlestr += " MeV";
-	const char * hTotEdepTitle = hTotEdepTitlestr.c_str();
-	const int numOfHistBins = atoi(argv[4]);
-	const int maxHistNum = atoi(argv[5]);
-	TH1D h_totedep("h_totedep", hTotEdepTitle, numOfHistBins, 0, maxHistNum);
+	// Set up histograms
+	TH1D h_totedep(hist_totedep_name, h_edep_title, hist_bins_and_xlim, 0, hist_bins_and_xlim);
+	TH1D h_effedep(hist_eff_name, h_edep_title, hist_bins_and_xlim, 0, hist_bins_and_xlim);
 
-	h_totedep.GetXaxis()->SetTitle("E_dep (MeV)");
-	h_totedep.GetYaxis()->SetTitle("Events");
+	// Set up histogram axes titles
+	h_totedep.GetXaxis()->SetTitle(hist_x_label);
+	h_totedep.GetYaxis()->SetTitle(hist_y_label);
+	h_effedep.GetXaxis()->SetTitle(hist_x_label);
+	h_effedep.GetYaxis()->SetTitle(hist_eff_y_label);
 
-	std::string hEffEdepTitlestr = "Total ";
-	hEffEdepTitlestr += argv[2];
-	hEffEdepTitlestr += " E_dep - ";
-	hEffEdepTitlestr += argv[3];
-	hEffEdepTitlestr += " MeV";
-	const char * hEffEdepTitle = hEffEdepTitlestr.c_str();
-	TH1D h_effedep("h_effedep", hEffEdepTitle, numOfHistBins, 0, maxHistNum);
+	// The reason for kin_energy + 1 is due to us including 0
+	double eff_values[kin_energy+1];
+	double e_thresholds[kin_energy+1];
+	int events_detected[kin_energy+1];
+	for (int a = 0; a < kin_energy+1; a++) {
+ 		// Initialize
+		events_detected[a] = 0;
+		e_thresholds[a] = a;
+	}
 
-	h_effedep.GetXaxis()->SetTitle("E_dep (MeV)");
-	h_effedep.GetYaxis()->SetTitle("Events (with cut)");
-
-	TGraph g_avgTvsAvgZ(10000);
-	g_avgTvsAvgZ.SetTitle("AvgT vs AvgZ; avgT; avgZ");
 	try {
 		while (reader.next() == true) {
+			// THIS IS FOR ONE EVENT GOING THROUGH!
 			reader.read(event);
 
 			evCounter = evCounter + 1;
@@ -119,107 +144,56 @@ int main(int argc, char** argv) {
 			event.getStructure(bMCTrue);
 			event.getStructure(fluxADC);
 
-			int nPart = bMCTrue.getRows();
+			int nPart = bMCTrue.getRows(); // Equals number of particles in one event
 
 			// Get total number of Edep per event
-			float edep_sum;
-			float eff_edep_sum;
-			float e_threshold = 100;
-			//cout << size(bMCTrue.getFloat("totEdep")) << endl;
+			float edep_sum = 0;
+			float neutron_edep = 0;
+
 			for (auto i: bMCTrue.getFloat("totEdep")) {
 				int mpid = bMCTrue.getInt("mpid")[i];
 				int pid = bMCTrue.getInt("pid")[i];
-				edep_sum += i; // uncomment if you want all secondaries
-//				// Let's filter out the particles
-//				if (pid == proton_ID && M == PROTONMASS) { //proton
-//					//cout << "Particle ID:" << pid << endl;
-//					edep_sum += i;
-//				} else if (pid == neutron_ID && M == NEUTRONMASS) {
-//					edep_sum +=i;
-//				} else if (pid == electron_ID && M == ELECTRONMASS) {
-//					edep_sum +=i;
-//				} else if (pid == positron_ID && M == ELECTRONMASS) {
-//					edep_sum +=i;
-//				}
+				edep_sum += i; // All secondaries
+                if (pid == neutron_ID && M == NEUTRONMASS) {
+                	neutron_edep += i;	// Example: only receive neutron energy dep
+                }
 			}
-			int eff_events = 0;
-			if (edep_sum > e_threshold) {
-				eff_events += 1; // one event that reached threshold
+
+			// Code to calc efficiency based on different thresholds
+			for (int a = 0; a < kin_energy + 1; a++) {
+				if (edep_sum >= e_thresholds[a]) {
+					// This will count every event detected for each threshold
+					// Say the particle has an edep_sum of 50 MeV,
+					// it will then count events from 0 to 50 MeV
+					events_detected[a]++;
+				}
+			}
+
+			// Fill in histograms
+			h_totedep.Fill(edep_sum);
+			if (edep_sum > 100) {
 				h_effedep.Fill(edep_sum);
 			}
-			h_totedep.Fill(edep_sum);
+
+			// Clear edep_sum for next event
 			edep_sum = 0;
-			eff_edep_sum = 0;
 
-//			float avgT_sum;
-//			float avgZ_sum;
-//			for (auto i: bMCTrue.getFloat("avgT")) {	// Equals 10,000 entries
-//				avgZ_sum += bMCTrue.getFloat("avgZ")[i];
-//				int mpid = bMCTrue.getInt("mpid")[i];
-//				int pid = bMCTrue.getInt("pid")[i];
-//				// Let's filter out the particles
-//				if (pid == proton_ID && M == PROTONMASS) { //proton
-//					//cout << "Particle ID:" << pid << endl;
-//					avgT_sum += i;
-//				} else if (pid == neutron_ID && M == NEUTRONMASS) {
-//					avgT_sum +=i;
-//				} else if (pid == electron_ID && M == ELECTRONMASS) {
-//					avgT_sum +=i;
-//				} else if (pid == positron_ID && M == ELECTRONMASS) {
-//                    avgT_sum += i;
-//                }
-//			}
-//			g_avgTvsAvgZ.AddPoint(avgT_sum, avgZ_sum);
-			double Edep_slab1 = 0;
-			double Edep_slab2 = 0;
-//			vector<double> Edep_slab;
-			//vector<double> tot_Edep;
-//			vector<double> zpos;
-			int numOfSlabs = 21;
-			int gap = 3;
-			int leadThickness = 5; // mm
-			int slab1Thickness = 6;
-			int slab2Thickness = 5;
-
-			double x[nPart], y[nPart];
+			const int numOfSlabs = 21;
 
 			for (int ip = 0; ip < nPart; ip++) {
 
 				double avgZ = bMCTrue.getFloat("avgZ", ip);
 				double Edep = double(bMCTrue.getFloat("totEdep", ip));
-				double identifier = int(fluxADC.getInt("component", ip));
-				//h_totedep.Fill(Edep);
+				double identifier = int(fluxADC.getInt("component", ip)); // ID of scintillator
 
-
-				//tot_Edep.push(Edep);
-                double total_edep_layer[numOfSlabs];
-				// iterate through zpos and edep_slab arrays
+				double total_edep_layer[numOfSlabs];
+				// Iterate through zpos and edep_slab arrays
 				for (int n = 1; n < numOfSlabs; n++) {
-                    if (identifier == n) {
-                        total_edep_layer[n] += Edep;
-                    }
-//					// create zpos
-//					if (n == 1 || n == 2) {
-//						Edep_slab.push_back(0);
-//						zpos.push_back(zpos[n-1] + gap + slab1Thickness + gap + leadThickness + gap);
-//						//cout << "Zpos: " << zpos[n] << endl;
-//						//cout << "Edep_slab: " << Edep_slab[n] << endl;
-//						if (avgZ < zpos[n] - slab1Thickness) { Edep_slab[n] += Edep; }
-//						z
-//					} else {
-//						Edep_slab.push_back(0);
-//						zpos.push_back(zpos[n-1] + gap + leadThickness + gap + slab2Thickness);
-//						//cout << "Zpos: " << zpos[n] << endl;
-//						//cout << "Edep_slab: " << Edep_slab[n] << endl;
-//						if (avgZ < zpos[n] - slab2Thickness) { Edep_slab[n] += Edep; }
-//					}
+					if (identifier == n) {
+						total_edep_layer[n] += Edep; // Get total Edep for layer n
+					}
 				}
-				// cout << "Edep: " << Edep << endl;
-				// cout << "avgZ: " << avgZ << endl;
-				// // in slab1
-				// if (avgZ < zpos1 - 4) { Edep_slab1 += Edep; }
-				// // in slab2
-				// if (avgZ > zpos1 + 4 && avgZ < zpos2 - 4) { Edep_slab2 += Edep; }
+
 			}
 
 		}
@@ -228,7 +202,34 @@ int main(int argc, char** argv) {
 	{
 		cerr << msg << endl;
 	}
-	//tree->Branch("h_totedep", &h_totedep, 32000, 0);
+
+
+	int tot_events_detected = 0;
+
+	// After every event is finished processing, find efficiency for each event
+	for (int i = 0; i < kin_energy+1; i++) {
+		cout << "events_detected[" << i << "]" << " = " << events_detected[i] << endl;
+		eff_values[i] = events_detected[i]/total_events_gemc;
+		tot_events_detected += events_detected[i];
+
+		// Debugging - Carmack would hate this...
+		// and I agree it's bad, so use GDB to test values (don't be me)
+
+		cout << "eff_values[" << i << "]" << " = " << eff_values[i] << endl;
+
+		if (e_thresholds[i] < 2*kin_energy && eff_values[i] != 0) {
+			g_efficiency.AddPoint(i, eff_values[i]);
+		}
+	}
+	cout << tot_events_detected << endl;
+
+	g_efficiency.SetTitle(eff_graph_title);
+	//Optional: g_efficiency.Fit("gaus");
+
+	// Write Graphs
+	g_efficiency.Write(eff_graph_name);
+
+	// Write Histograms
 	gDirectory->Write();
 	return 0;
 }
--- phyTests/axial_neutrons/export-to-rootfile.sh
+++ phyTests/axial_neutrons/export-to-rootfile.sh
@@ -1,13 +1,17 @@
 if [ $# -eq 0 ] || [ $# -ne 4 ]
 	then
 		echo "Not enough arguments supplied. Specify particle first [proton, neutron, electron, etc.]..."
+		echo "Specify kinetic energy"
 		echo "Then, specify number of bins and max value of histogram plot..."
 		exit
 fi
+echo "${PWD}"
+# $1 is file name, $2 is particle, $3 the kinetic energy, $4 is amount histogram bins/range
 rp=$(realpath $1)
 dn=$(dirname $rp)
 fn=$(basename $rp .hipo)
+echo "==Making root file ${fn}.root=="
 echo "Analysis for $1:"
 cd ../build
-make && cd $dn && ../build/analysis $1 $2 $fn $3 $4
+make && cd $dn && ../build/analysis $1 $2 $3 $4 $fn
 echo "Completed file $1:"
